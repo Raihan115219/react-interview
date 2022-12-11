5be8eb1fad3ac20cb4caa2dfe319c62efd2e7db9
Raihan115219 <aburaihan115219@gmail.com>
2022-12-12 03:59:08 +0600

    final done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Dropdown } from "react-bootstrap";
+import { Button, Container, Dropdown } from "react-bootstrap";
 import "./Header.css";
 import { BiUserCircle, BiWallet } from "react-icons/bi";
 import { FaEthereum, FaWallet } from "react-icons/fa";
@@ -20,86 +20,72 @@ function Header() {
           </NavLink>
           <div className="infoBtn">
             {/* coin seletions */}
-            <div className="etherume me-2">
-              <Dropdown className="me-4">
-                <Dropdown.Toggle variant="none" id="dropdown-basic">
-                  <div className="d-flex justify-content-center align-items-center ">
-                    <FaEthereum className="me-1 fs-4" />
-                    <p className=" d-lg-block d-sm-none d-md-none">
-                      Ethereum Kovan
-                    </p>
-                  </div>
+            <div className="me-2">
+              <Dropdown>
+                <Dropdown.Toggle
+                  variant="none"
+                  id="dropdown-basic"
+                  className="coinBtn"
+                >
+                  <FaEthereum />
+                  <span className="coinName">Etherume Kovan</span>
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
-                  {coin?.map((coin) => {
+                  {coin?.map((coin, i) => {
                     return (
-                      <div>
-                        <Dropdown.Item
-                          href={`#/action-${coin.id}`}
-                          key={coin.id}
-                          className="me-1"
-                        >
-                          {coin.icon}
-                          {coin.name}
-                        </Dropdown.Item>
-                      </div>
+                      <Dropdown.Item href="#/action-1">
+                        {coin.icon} {coin.name}
+                      </Dropdown.Item>
                     );
                   })}
                 </Dropdown.Menu>
               </Dropdown>
             </div>
             {/* wallet check */}
-            <div
-              className="waletConnect me-2"
-              onClick={() => setModalShow(true)}
-            >
-              <div>
-                <FaWallet className="fs-5 me-1 walletIcon" />
-              </div>
-              <div>
-                <p className="d-lg-block d-sm-none d-md-none">Wallet Connect</p>
-              </div>
+            <div className="me-2">
+              <Button
+                variant="none"
+                size="md"
+                className="connectWallet"
+                onClick={() => setModalShow(true)}
+              >
+                <FaWallet /> <span className="coinName">Connect wallet</span>
+              </Button>
             </div>
             {/* user part */}
-            <div className="userPart  me-2">
-              <div className="icon">
-                <Dropdown className="toggle">
-                  <Dropdown.Toggle
-                    variant="none"
-                    id="dropdown-basic"
-                    className="toggle"
-                  >
-                    <BiUserCircle className="userIcon" />
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>
-                      <NavLink
-                        to="/login"
-                        className="text-decoration-none text-primary"
-                      >
-                        Login
-                      </NavLink>
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      <NavLink
-                        to="/signup"
-                        className="text-decoration-none text-primary"
-                      >
-                        Signup
-                      </NavLink>
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      <NavLink
-                        to="/faq"
-                        className="text-decoration-none text-primary"
-                      >
-                        FAQ
-                      </NavLink>
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
+            <div className="me-2">
+              <Dropdown>
+                <Dropdown.Toggle variant="none" id="dropdown-basic">
+                  <BiUserCircle className="userIcon" />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item>
+                    <NavLink
+                      to="/login"
+                      className="text-decoration-none text-primary"
+                    >
+                      Login
+                    </NavLink>
+                  </Dropdown.Item>
+                  <Dropdown.Item>
+                    <NavLink
+                      to="/signup"
+                      className="text-decoration-none text-primary"
+                    >
+                      Signup
+                    </NavLink>
+                  </Dropdown.Item>
+                  <Dropdown.Item>
+                    <NavLink
+                      to="/faq"
+                      className="text-decoration-none text-primary"
+                    >
+                      FAQ
+                    </NavLink>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
           </div>
         </div>
